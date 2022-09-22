b54fec6f0ad95b368a95c87e695b93bef7bec7fe
Anuradha <anusrini2017@gmail.com>
2022-09-22 14:44:32 +0530

    javascript file updated

--- javascript/main.js
+++ javascript/main.js
@@ -2,29 +2,32 @@
  
 var para1= document.createElement("P");
 para1.innerHTML="<h3>&nbsp;&lt;This is Main Javascript File</h3>";
-
+document.body.append(para1);
 function scriptLoad(src)
 {
- 
+
 var script=document.createElement("script");
-script.src=src;
-document.body.append(para1);
-document.head.append(script);
+ script.src=src;
+ document.body.append(para1);
+ document.head.append(script);
 
 }
 
 //passing the value of src as secondfile.js
 function externalScriptLoad(){
 scriptLoad("/js/secondfile.js");
-//scriptLoad("/javascript/thirdfile.js");
+
 }
 
 //loading extenal file like bootstrap and jquery
 
 function thirdpartyScriptLoad()
 {
+
 scriptLoad("https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js");
 scriptLoad("https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js");
+
+
 }
 
 
