f6a3ce50d73222e3203eed1da7fbdbb199fb08f0
Anuradha <anusrini2017@gmail.com>
2022-09-22 13:01:07 +0530

    final modification of communications

--- routes/communications.js
+++ routes/communications.js
@@ -14,32 +14,38 @@ router.post("/", function (req, res) {
 
 
 router.post("/ajax", function (req, res) {
-   res.send("Form submitted using ajax method")
+   res.send("Form submitted using ajax method");
+   console.log(req.body);
    
 });
 
-// router.get("/fetch", function (req, res) {
-//    //res.send("Form submitted using fetch")
-//    res.json({ "method": "fetch" });
-// });
+router.get("/fetch", function (req, res) {
+   //res.send("Form submitted using fetch")
+   res.json({ "method": "fetch" });
+});
 router.post("/fetch", function (req, res) {
-      console.log(req.body["msg"]);
-      res.send((req.body["msg"]));
+   res.send((req.body["msg"]));
 });
 router.post("/axios", function (req, res) {
-   res.json(req.body["method"]);
+   //res.json(req.body["method"]);
+   var data=req.body["method"];
+   res.send(`Form submitted using ${data} method`);
 
 })
 router.post("/sendbeacon", function (req, res) {
-      res.send(req.body["method"]);
+   res.send("Form submitted using sendbeacon method");
 })
 router.post("/jquery", function (req, res) {
    var data = req.body["data"];
-   res.send(`Form submitted using ${data}`);
+   res.send(`Form submitted using ${data} method`);
 });
 
 router.get("/window-open", function (req, res) {
    res.sendFile(path.join(__dirname, "../communications/window-open.html"));
+});
+
+router.get("/childwindow",function(req,res){
+   res.sendFile(path.join(__dirname,"../communications/childwindow.html"));
 
 });
 
